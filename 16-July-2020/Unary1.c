08aed45f5458a711af0e1eee8fd888bd67024477
Pranali <pranalipatil3620@gmail.com>
2020-07-20 00:53:59 +0530

    lecture

---
+++ 16-July-2020/Unary1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+
+        void main() {
+
+                int num=10,ans=0;
+
+                ans = ++num + ++num;
+        
+		printf("ans = %d\n",ans);
+                printf("num = %d\n",num);
+        }
---
+++ 16-July-2020/Unary1.java
@@ -0,0 +1,11 @@
+class UnaryDemo{
+
+	public static void main(String[] args) {
+	
+		int num=10,ans=0;
+
+		ans = ++num + ++num;
+	       	System.out.println("ans = "+ans);
+		System.out.println("num = "+num);	
+	}
+}
